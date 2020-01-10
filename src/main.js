655511d4a7781c365ffbe064a3a115a4d3ddc86f
François Lévesque <francois@witify.io>
2020-01-10 15:32:22 -0500

    Add i18n

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import store from "./store";
 import helpers from "./services/helpers";
 import "./assets/tailwind.css";
 import "vue-scrollama/dist/vue-scrollama.css";
+import i18n from "./i18n";
 
 Vue.config.productionTip = false;
 
 new Vue({
 	store,
+	i18n,
 	render: h => h(App)
 }).$mount("#app");
