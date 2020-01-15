340757d5af41d3b8021eb55bdaece80288f7f7bc
François Lévesque <francois@witify.io>
2020-01-15 16:19:16 -0500

    remove dependencies

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
-import store from "./store";
-import helpers from "./services/helpers";
-import "./assets/tailwind.css";
-import Switch from "./components/inputs/Switch.vue";
-import "vue-scrollama/dist/vue-scrollama.css";
 import i18n from "./i18n";
+import Switch from "./components/inputs/Switch.vue";
+import "./assets/tailwind.css";
+import "vue-scrollama/dist/vue-scrollama.css";
 
 Vue.config.productionTip = false;
 
 Vue.component("v-switch", Switch);
 
 new Vue({
-	store,
 	i18n,
 	render: h => h(App)
 }).$mount("#app");
