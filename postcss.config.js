02e9da216ca4a52c2750ff5c51c054adadfaf7c8
François Lévesque <francois@witify.io>
2020-01-15 16:37:19 -0500

    Fix purges

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
 	plugins: {
 		tailwindcss: {},
-		"vue-cli-plugin-tailwind/purgecss": {}
+		"vue-cli-plugin-tailwind/purgecss": {
+			whitelist: ["bg-red-500", "bg-blue-500", "bg-green-500", "bg-purple-500", "bg-teal-500", "bg-blue-900", "bg-blue-800", "bg-blue-200", "bg-blue-100", "fill-current", "text-xs", "text-sm"],
+			whitelistPatterns: [/scrollama/, /text-blue/, /text-teal/, /text-purple/, /text-green/, /text-red/]
+		},
 	}
 };
