8414f8f4bd8237bd87021c146ca5d9d3d097bef8
François Lévesque <francois@witify.io>
2020-01-15 16:21:59 -0500

    remove dependencies

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
-import i18n from "./i18n";
-import Switch from "./components/inputs/Switch.vue";
+import helpers from "./services/helpers"; // IMPORTANT TO KEEP
 import "./assets/tailwind.css";
+import Switch from "./components/inputs/Switch.vue";
 import "vue-scrollama/dist/vue-scrollama.css";
+import i18n from "./i18n";
 
 Vue.config.productionTip = false;
 
